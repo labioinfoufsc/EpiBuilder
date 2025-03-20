3e90c54ffbe8629db838c75debb57ae1349a7488
russi <bruna.russi@posgrad.ufsc.br>
2025-03-20 08:49:16 -0300

    chore: create base structure
    Initial folder structure and basic setup for the epibuilder-frontend project.

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'EpiBuilder 2.0.0';
+}
